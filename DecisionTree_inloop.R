aae829adedd4d92298ee21659f25bb3e4918fe10
nazneensiddique <58657487+nazneensiddique@users.noreply.github.com>
2019-12-28 18:03:24 +0530

    Update DecisionTree_inloop.R

--- DecisionTree_inloop.R
+++ DecisionTree_inloop.R
@@ -1,3 +1,4 @@
+#BAGGING use LOOP
 acc<-c()
 for(i in 1:500)
 {
@@ -16,3 +17,27 @@ for(i in 1:500)
 }
 summary(acc)
 plot(fittree)
+
+
+
+#ADABOoSTING
+data("iris")
+#install.packages("caret")
+#install.packages("C50")
+library(caret)
+library(C50)
+
+inTraininglocal<-createDataPartition(iris$Species,p=.75,list = F)
+training<-iris[inTraininglocal,]
+testing<-iris[-inTraininglocal,]
+#Model Building
+model<-C5.0(training$Species~.,data = training, trials=10) #Trials- Boosting parameter
+
+#Generate the model summary
+summary(model)
+#Predict for test data set
+pred<-predict.C5.0(model,testing[,-5])
+a<-table(testing$Species,pred)
+sum(diag(a))/sum(a)
+plot(model)
+
